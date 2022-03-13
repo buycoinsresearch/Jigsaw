05a25cad4b69327d86d83fa262ab4a4f0959ef28
Jelilat Anofiu <anofiujelailah@gmail.com>
2022-03-13 12:15:49 +0100

    fix solution verification

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ As the name suggests, Jigsaw is based off the popular *tiling puzzle that requir
 When you create a Jigsaw, you create a bag of NFTs that contains different pieces of a Jigsaw puzle. Holders are expected to arrange the pieces to complete the puzzle.
 
 Only those who have completed the puzzle can claim the Jigsaw. The puzzle is completed when all the pieces are in the correct place.
+
+## Smart Contract
+
+The Jigsaw Smart Contract is currently deployed to the rinkeby testnet. It's available at the following address:
+
+(https://rinkeby.etherscan.io/address/0x60adBd2EFb2E65034df80882e1146e82f519A5A2)[`0x60adBd2EFb2E65034df80882e1146e82f519A5A2`]
--- contracts/Jigswa.sol
+++ contracts/Jigswa.sol
@@ -11,35 +11,45 @@ contract Jigsaw is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    mapping (uint => string) solutions;
+    struct tokenDetails {
+        bytes32 solution;
+        string uri;
+        bool status;
+    }
+
+    mapping (uint => tokenDetails) public solutions;
+    uint total;
 
     constructor() ERC721("Fractional NFT", "FRACTION") {
     }
 
-    function createNFTs(string[] memory solution, string[] memory uri) public returns(bool) {
+    function createNFTs(bytes32[] memory solution, string[] memory uri) public returns(bool) {
         require(solution.length == uri.length, "Solution and URI must be the same length");
         
         for (uint i = 0; i < solution.length; i++) {
-            _tokenIds.increment();
+            uint newItemId = total +1;
 
-            uint newItemId = _tokenIds.current();
-
-            _mint(address(this), newItemId);
-            _setTokenURI(newItemId, uri[i]);
-
-            solutions[newItemId] = solution[i];
+            solutions[newItemId] = tokenDetails(
+                solution[i],
+                uri[i],
+                false
+            );
         }
 
         return true;
     }
 
     function completePuzzle(uint tokenId, string memory solution)
-        public
-        returns (bool)
+        public returns (bool)
     {
-        require(keccak256(abi.encodePacked(solution)) == keccak256(abi.encodePacked(solutions[tokenId])), "Incorrect solution");
+        bytes memory sol = bytes(solution);
+        bytes32 esol = sha256(sol);
+        require(esol == solutions[tokenId].solution, "Incorrect Solution");
         
-        transferFrom(address(this), msg.sender, tokenId);
+        _tokenIds.increment();
+        uint newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, solutions[tokenId].uri);
 
         return true;
     }
